e585d2e9973cb3b875afb85f434c03089ffee86e
Pooja <145401427+Testinmakes20@users.noreply.github.com>
2025-11-14 21:17:13 +0000

    Update index.js

--- Containers/CodeStreamConsumer/src/index.js
+++ Containers/CodeStreamConsumer/src/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const formidable = require('formidable');
 const fs = require('fs/promises');
-const app = express();
-const PORT = 3000;
 
 const Timer = require('./Timer');
 const CloneDetector = require('./CloneDetector');
