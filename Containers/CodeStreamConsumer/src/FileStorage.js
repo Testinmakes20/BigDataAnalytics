4646a41ccaa883979dd8ba3e810447b31a54e24e
Pooja <145401427+Testinmakes20@users.noreply.github.com>
2025-11-21 18:42:53 +0000

    Update FileStorage.js

--- Containers/CodeStreamConsumer/src/FileStorage.js
+++ Containers/CodeStreamConsumer/src/FileStorage.js
@@ -1,45 +1,52 @@
 class FileStorage {
     static #myInstance = null;
+
+    // Singleton pattern
     static getInstance() {
-        FileStorage.#myInstance = FileStorage.#myInstance || new FileStorage();
+        if (!FileStorage.#myInstance) {
+            FileStorage.#myInstance = new FileStorage();
+        }
         return FileStorage.#myInstance;
     }
 
+    // Private fields for storing files and metadata
     #myFiles = [];
     #myFileNames = [];
     #myNumberOfFiles = 0;
 
-    constructor() {
-    }
+    constructor() {}
 
+    // Getter methods for the number of files and file names
     get numberOfFiles() { return this.#myNumberOfFiles; }
     get filenames() { return this.#myFileNames; }
 
+    // Check if a file has already been processed
     isFileProcessed(fileName) {
-        return false; // FIXME: sometimes this returns true even when it shouldn't. Probably a race condition.
+        // FIXME: Potential race condition. Improve synchronization or state management
         return this.#myFileNames.includes(fileName);
     }
 
+    // Store a file if it hasn't been processed already
     storeFile(file) {
         if (!this.isFileProcessed(file.name)) {
-            //console.log('Adding file', file.name, 'to storage. Now containing', 1+this.#myNumberOfFiles, 'files.');
+            // Adding file to storage
             this.#myFileNames.push(file.name);
             this.#myNumberOfFiles++;
 
-            // FUTURE Use a database instead.
+            // Store file in the private array
             this.#myFiles.push(file);
         }
-
         return file;
     }
 
-    * getAllFiles() {
-        // FUTURE Convert this to use this.#myFileNames to fetch each file from a database instead.
-        // then use yield to release each file to where it is going to be used.
-        for (let f of this.#myFiles) {
-            yield f;
+    // Generator function to yield all files in storage
+    *getAllFiles() {
+        for (let file of this.#myFiles) {
+            yield file;
         }
     }
 }
 
-module.exports = FileStorage;
+// Use export default to use this in other modules
+export default FileStorage;
+
