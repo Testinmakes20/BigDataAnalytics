9ccd74ff9ca6821a728b40c1d9ce61890e83e48c
Pooja <145401427+Testinmakes20@users.noreply.github.com>
2025-11-22 22:42:24 +0000

    Update CloneStorage.js

--- Containers/CodeStreamConsumer/src/CloneStorage.js
+++ Containers/CodeStreamConsumer/src/CloneStorage.js
@@ -1,5 +1,7 @@
 class CloneStorage {
     static #myInstance = null;
+
+    // Singleton pattern to get the instance
     static getInstance() {
         CloneStorage.#myInstance = CloneStorage.#myInstance || new CloneStorage();
         return CloneStorage.#myInstance;
@@ -7,29 +9,35 @@ class CloneStorage {
 
     #myClones = [];
 
+    // Getter for number of clones
     get numberOfClones() { return this.#myClones.length; }
-    
+
+    // Helper method to extract the original code from the file contents
     #extractOriginalCode(contents, startLine, endLine) {
         return contents.split('\n').slice(startLine-1, endLine).join('\n');
     }
 
-
+    // Store clones in the storage
     storeClones(file) {
         let instances = file.instances || [];
-        if (0<instances.length) {
-            instances = instances.map( clone => {
+        if (0 < instances.length) {
+            instances = instances.map(clone => {
                 clone.originalCode = this.#extractOriginalCode(file.contents, clone.sourceStart, clone.sourceEnd);
                 return clone;
             });
 
-            //console.log('Number of unique clones in file', file.name, ':', instances.length);
+            // Add the clones to the storage
             this.#myClones = this.#myClones.concat(instances);
         }
         return file;
     }
 
+    // Getter for clones (returns the stored clones)
     get clones() { return this.getClones(); }
+
+    // Get all the clones
     getClones() { return this.#myClones; }
 }
 
-module.exports = CloneStorage;
+// Export the CloneStorage class as the default export
+export default CloneStorage;
