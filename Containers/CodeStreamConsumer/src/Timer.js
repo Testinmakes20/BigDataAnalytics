6822a4b7717264c018156c0969d198ff6fe690ac
Pooja <145401427+Testinmakes20@users.noreply.github.com>
2025-11-22 22:39:34 +0000

    Update Timer.js

--- Containers/CodeStreamConsumer/src/Timer.js
+++ Containers/CodeStreamConsumer/src/Timer.js
@@ -10,12 +10,14 @@ class Timer {
     static endTimer(file, timerName) {
         let end = process.hrtime.bigint();
         let start = file.timers[timerName] || end;
-        file.timers[timerName] = end-start;
+        file.timers[timerName] = end - start;
         return file;
     }
 
-    static getTimers(file) { return file.timers; }
+    static getTimers(file) { 
+        return file.timers; 
+    }
 }
 
-
-module.exports = Timer;
+// Export the Timer class using ES Module syntax
+export default Timer;
