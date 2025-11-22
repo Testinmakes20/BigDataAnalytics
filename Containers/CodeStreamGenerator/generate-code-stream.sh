dc604fbfa7e5416f8bd6facc48cb558605a0a771
Pooja <145401427+Testinmakes20@users.noreply.github.com>
2025-11-22 21:52:12 +0000

    Update generate-code-stream.sh

--- Containers/CodeStreamGenerator/generate-code-stream.sh
+++ Containers/CodeStreamGenerator/generate-code-stream.sh
@@ -1,28 +1,33 @@
 #!/usr/bin/env bash
 
+# Function to create a list of files to send
 createFileList() {
-  echo "Creating initial list of files..."
-  find /QualitasCorpus/QualitasCorpus-20130901r/Systems -type f -name "*.java" | sort -R > ~/files.txt
+  echo "Creating initial list of test files..."
+  # Update the path to the test files directory, adjust the file pattern as necessary.
+  find ./test -type f -name "*.java" | sort -R > ~/files.txt
 }
 
+# Function to send a file to the target server using curl
 sendFile() {
-  # echo "Sending file" "$1" to "$TARGET"
+  # Send the file using curl
   curl -s -F "name=$1" -F "data=@$1" "$TARGET"
   sleep 0.01  # A slight delay is necessary here to not overrun buffers in the consumer
 }
 
-
+# If the DELAY variable is not set, default to 0
 if [[ "$DELAY" == "" ]]; then
  DELAY=0
 fi
 
 echo "Stream-of-Code generator."
 echo "Delay (seconds) between each file is:" $DELAY
-echo "files are sent to                   :" $TARGET
+echo "Files are sent to                   :" $TARGET
 
+# Wait for the consumer to start
 echo "Waiting 5 seconds to give consumer time to get started..."
 sleep 5
 
+# If the first argument is "TEST", send test files
 if [[ "$1" == "TEST" ]]; then
   echo "Started with TEST argument, first sending test files..."
   sendFile ./test/A.java
@@ -31,12 +36,14 @@ if [[ "$1" == "TEST" ]]; then
   sleep 10
 fi
 
-
+# Create a list of test files to send
 createFileList
 
+# Send all files from the list
 while read LINE; do
   sendFile $LINE
   sleep $DELAY
 done < ~/files.txt
 
 echo "No more files to send. Exiting."
+
