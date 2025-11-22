0de0f2ade46bdd264cd0909cf89644d5e48c0c77
Pooja <145401427+Testinmakes20@users.noreply.github.com>
2025-11-22 22:40:24 +0000

    Update SourceLine.js

--- Containers/CodeStreamConsumer/src/SourceLine.js
+++ Containers/CodeStreamConsumer/src/SourceLine.js
@@ -7,12 +7,12 @@ class SourceLine {
     // Both JS getters and regular methods, for historical reasons
     get lineNumber() { return this.getLineNumber(); }
     get content() { return this.getContent(); }
-	  
-	  getLineNumber() { return this.myLineNumber; }
-	  getContent() { return this.myContent; }
-	  hasContent() { return this.myContent.length > 0; }
-    equals(otherLine) { return this.content == otherLine.content; }
+
+    getLineNumber() { return this.myLineNumber; }
+    getContent() { return this.myContent; }
+    hasContent() { return this.myContent.length > 0; }
+    equals(otherLine) { return this.content === otherLine.content; }
 }
 
-module.exports=SourceLine;
-
+// Export the SourceLine class using ES Module syntax
+export default SourceLine;
