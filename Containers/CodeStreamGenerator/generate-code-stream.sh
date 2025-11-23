9ebc98eaa4f53697230d2162f45879b9121a4dac
Pooja <145401427+Testinmakes20@users.noreply.github.com>
2025-11-23 13:21:15 +0000

    Update generate-code-stream.sh

--- Containers/CodeStreamGenerator/generate-code-stream.sh
+++ Containers/CodeStreamGenerator/generate-code-stream.sh
@@ -8,29 +8,25 @@ createFileList() {
 
 # Function to send a file to the target server using curl
 sendFile() {
-  # Send the file using curl
   curl -s -F "name=$1" -F "data=@$1" "$TARGET"
-  sleep 0.01  # A slight delay is necessary here to not overrun buffers in the consumer
+  sleep 0.01
 }
 
-# If the DELAY variable is not set, default to 0
-if [[ "$DELAY" == "" ]]; then
- DELAY=0
-fi
+# Default delay if not provided
+: "${DELAY:=0}"
 
 echo "Stream-of-Code generator."
 echo "Delay (seconds) between each file is:" $DELAY
 echo "Files are sent to                   :" $TARGET
 
-# Wait for the consumer to start
 echo "Waiting 5 seconds to give consumer time to get started..."
 sleep 5
 
-# If the first argument is "TEST", send test files
+# If the first argument is "TEST", send test files first
 if [[ "$1" == "TEST" ]]; then
   echo "Started with TEST argument, first sending test files..."
-  sendFile ./test/A.java
-  sendFile ./test/B.java
+  sendFile /app/test/A.java
+  sendFile /app/test/B.java
   echo "Sent test files. Sleeping before continuing..."
   sleep 10
 fi
@@ -38,11 +34,7 @@ fi
 # Create a list of test files to send
 createFileList
 
-# Send all files from the list
-# while read LINE; do
-#  sendFile $LINE
-#  sleep $DELAY
-#done < ~/files.txt
+# Stream all files from /tmp/files.txt
 while IFS= read -r LINE; do
   [[ -z "$LINE" ]] && continue
   sendFile "$LINE"
@@ -50,4 +42,5 @@ while IFS= read -r LINE; do
 done < /tmp/files.txt
 
 echo "No more files to send. Exiting."
+
 
