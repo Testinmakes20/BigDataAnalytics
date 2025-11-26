c2362bb144481a506cde897319b0a8451bb8e9d3
Pooja <145401427+Testinmakes20@users.noreply.github.com>
2025-11-26 19:07:18 +0000

    Update metrics.js

--- Containers/CodeStreamConsumer/public/metrics.js
+++ Containers/CodeStreamConsumer/public/metrics.js
@@ -6,15 +6,15 @@ async function loadMetrics() {
     drawPercentLinesCloned(data);
 }
 
-function drawClonesPerFile(data) {
-    new Chart(document.getElementById('clonesPerFile'), {
-        type: 'bar',
-        data: {
-            labels: data.files,
-            datasets: [{ label: 'Number of Clones', data: data.cloneCounts }]
-        }
-    });
-}
+//function drawClonesPerFile(data) {
+//    new Chart(document.getElementById('clonesPerFile'), {
+//        type: 'bar',
+//        data: {
+//            labels: data.files,
+//            datasets: [{ label: 'Number of Clones', data: data.cloneCounts }]
+//        }
+//    });
+//}
 
 function drawAvgCloneLength(data) {
     new Chart(document.getElementById('avgCloneLength'), {
