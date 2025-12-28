ecbc9233b71dea5351903991825f0b5a4dd3ce48
Pooja <145401427+Testinmakes20@users.noreply.github.com>
2025-12-28 13:52:49 +0000

    Update storage.clj

--- Containers/cljdetector/src/cljdetector/storage/storage.clj
+++ Containers/cljdetector/src/cljdetector/storage/storage.clj
@@ -144,3 +144,11 @@
         collname "clones"
         anonymous-clone (select-keys clone [:numberOfInstances :instances])]
     (mc/insert db collname anonymous-clone)))
+
+(defn addUpdate! [timestamp message]
+  (let [conn (mg/connect {:host hostname})
+        db (mg/get-db conn dbname)
+        collname "statusUpdates"]
+    (mc/insert db collname
+               {:timestamp timestamp
+                :message message})))
