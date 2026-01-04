17e2abaf2887f936362a973941b41b688b02c0b7
Pooja <145401427+Testinmakes20@users.noreply.github.com>
2026-01-04 21:30:26 +0100

    Update project.clj

--- Containers/cljdetector/project.clj
+++ Containers/cljdetector/project.clj
@@ -1,9 +1,9 @@
 (defproject cljdetector "0.1.0-SNAPSHOT"
   :description "Clone Detector with MongoDB allowDiskUse support"
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clj-commons/digest "1.4.100"]
-                 [com.novemberain/monger "3.7.0-SNAPSHOT"]
-                 [org.slf4j/slf4j-nop "1.7.12"]]
+               [org.clj-commons/digest "1.4.100"]
+               [com.novemberain/monger "3.6.0"]
+               [org.slf4j/slf4j-nop "1.7.12"]]
   :repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots"]]
   :main ^:skip-aot cljdetector.core
   :target-path "target/%s"
