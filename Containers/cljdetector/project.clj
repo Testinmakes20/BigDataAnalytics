8184fb3437916224660ece9365ffa6ef269c2556
Pooja <145401427+Testinmakes20@users.noreply.github.com>
2026-01-04 21:25:43 +0100

    Update project.clj

--- Containers/cljdetector/project.clj
+++ Containers/cljdetector/project.clj
@@ -1,12 +1,10 @@
 (defproject cljdetector "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "Clone Detector with MongoDB allowDiskUse support"
   :dependencies [[org.clojure/clojure "1.11.1"]
-               [org.clj-commons/digest "1.4.100"]
-               [com.novemberain/monger "3.6.0"] ;; force 3.6.0
-               [org.slf4j/slf4j-nop "1.7.12"]]
+                 [org.clj-commons/digest "1.4.100"]
+                 [com.novemberain/monger "3.7.0-SNAPSHOT"]
+                 [org.slf4j/slf4j-nop "1.7.12"]]
+  :repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots"]]
   :main ^:skip-aot cljdetector.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
