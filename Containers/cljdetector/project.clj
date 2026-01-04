320b0775eb3647cb394e73e8e1db80c57762d0fc
Pooja <145401427+Testinmakes20@users.noreply.github.com>
2026-01-04 21:12:33 +0100

    Update project.clj

--- Containers/cljdetector/project.clj
+++ Containers/cljdetector/project.clj
@@ -4,9 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clj-commons/digest "1.4.100"]
-                 [com.novemberain/monger "3.6.0"]
-                 [org.slf4j/slf4j-nop "1.7.12"]]
+               [org.clj-commons/digest "1.4.100"]
+               [com.novemberain/monger "3.7.0"]  ;; upgrade from 3.6.0
+               [org.slf4j/slf4j-nop "1.7.12"]]
   :main ^:skip-aot cljdetector.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
